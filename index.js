56a74f5c05708a6f3024498d1b9ef8d219f85710
unknown <onurcoskun99@gmail.com>
2021-01-03 18:53:48 +0300

    server update

--- index.js
+++ index.js
@@ -1,8 +1,4 @@
-// const { ApolloServer } = require('apollo-server-express');
-const { ApolloServer } = require('apollo-server')
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const { ApolloServer } = require('apollo-server');
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typedefs/typedefs');
 const resolvers = require('./graphql/resolvers/index')
@@ -14,38 +10,22 @@ require('dotenv').config();
 const server = new ApolloServer({
     typeDefs,
     resolvers,
-    // cors: {
-    //     origin: '*',			// <- allow request from all domains
-    //     credentials: true
-    // },
-    cors: false,
     context: ({ req }) => ({
         req
-    }),
-
+    })
 })
-
-// const app = express();
-
-// app.use(cors())
-// app.use(bodyParser.json())
-// app.use("/images", express.static('public/images'))
-// server.applyMiddleware({ app })
-// app.listen(Port, (req, res) => {
-//     console.log(`server is running`)
-// })
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
     if (err) {
         console.log(err.message);
     } else {
         console.log('mongodb connected')
-        // app.listen(Port, (req, res) => {
-        //     console.log(`server is running`)
-        // })
-        server.listen(Port).then(
-            res => console.log(res.url, 'server is running')
-        ).catch(err => console.log(err))
+        server.listen(Port, (req, res) => {
+
+        }).then((res) => {
+            console.log(`server in running on ${res.url}`)
+        }).catch(err => {
+            console.log(err)
+        })
     }
 
 })
-
