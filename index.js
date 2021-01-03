219cc31733cee2f9972abc2a9568f1daba0896d4
unknown <onurcoskun99@gmail.com>
2021-01-03 18:13:43 +0300

    body parser

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const { ApolloServer } = require('apollo-server-express');
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typedefs/typedefs');
 const resolvers = require('./graphql/resolvers/index')
@@ -15,24 +16,25 @@ const server = new ApolloServer({
     context: ({ req }) => ({
         req
     }),
-    cors: {
-        origin: '*',
-        credentials: true
-    },
 })
 
 const app = express();
-server.applyMiddleware({ app })
-app.use(express.static('public'))
+
 app.use(cors())
+app.use(bodyParser.json())
+app.use("/images", express.static('public/images'))
+server.applyMiddleware({ app })
+app.listen(Port, (req, res) => {
+    console.log(`server is running`)
+})
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
     if (err) {
         console.log(err.message);
     } else {
         console.log('mongodb connected')
-        app.listen(Port, (req, res) => {
-            console.log(`server in running`)
-        })
+        // app.listen(Port, (req, res) => {
+        //     console.log(`server is running`)
+        // })
     }
 
 })
