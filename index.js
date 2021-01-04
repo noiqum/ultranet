b757ab7f7f6177cd9f35dad08ef03f98b590951c
unknown <onurcoskun99@gmail.com>
2021-01-05 00:36:06 +0300

    cors options try

--- index.js
+++ index.js
@@ -7,9 +7,15 @@ const Port = process.env.PORT || 5000;
 require('dotenv').config();
 //////////////////////////////////////
 
+
+
 const server = new ApolloServer({
     typeDefs,
     resolvers,
+    cors: {
+        origin: 'http://localhost:3000',
+        credentials: true
+    },
     context: ({ req }) => ({
         req
     })
