731a9d5991d0a8ce7a495879e0d05c357e423399
unknown <onurcoskun99@gmail.com>
2021-01-13 22:37:13 +0300

    cors middleware added due to fix cors bug

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { ApolloServer } = require('apollo-server-express');
 const mongoose = require('mongoose');
 const typeDefs = require('./graphql/typedefs/typedefs');
-const resolvers = require('./graphql/resolvers/index')
+const resolvers = require('./graphql/resolvers/index');
+const cors = require('cors')
 ////////////////////////////////////////
 const Port = process.env.PORT || 5000;
 require('dotenv').config();
@@ -21,7 +22,8 @@ const server = new ApolloServer({
     })
 })
 const app = express()
-server.applyMiddleware({ app, cors: corsOptions });
+app.use(cors())
+server.applyMiddleware({ app });
 
 mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }, (err) => {
     if (err) {
